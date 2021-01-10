7bebb12fcd988b9cd4d63467b88ad7bf524c07fa
VeraMG98 <rusyamg@mail.ru>
2021-01-10 20:04:32 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android2_1/ui/home/HomeFragment.java
@@ -0,0 +1,35 @@
+package com.example.android2_1.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.android2_1.R;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.fab).setOnClickListener(v -> openForm());
+    }
+
+    private void openForm() {
+
+    }
+}
